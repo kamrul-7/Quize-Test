9e2f51b235c8d66783bd29371a39b5b8d2dc1a3c
kamrul-7 <kamrul7.khan@gmail.com>
2022-10-12 20:47:03 +0600

    Quiz

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
     const post = posts.data;
     return (
         <div>
-            <div className='text-start m-10 title p-4 text-white'>
-                <h4 className='font-bold text-4xl text-info'>We believe in making knowledge fun for everyone.</h4>
+            <div className=' m-10 title pl-80 py-24 text-sky-800 rounded font-semibold'>
+                <h4 className='font-bold text-4xl text-red-700 my-5'> <i>We believe in making  knowledge <br />fun for everyone.</i></h4>
                 <p>What makes Sporcle truly special is the diverse community of people who make up our quiz community. Online, an average of over one million quizzes are played daily. Offline, over 15,000 people participate in our live events each week. For every Sporcler, our purpose is to create mentally stimulating diversions that bring them together.</p>
             </div>
-            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8 m-8'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8 m-8 mb-28'>
                 {
                     post.map(data => <Post
                         key={data.id}
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,10 +7,12 @@ const Post = ({ data }) => {
     return (
         <div className="card card-compact bg-base-100 shadow-xl p-2">
             <figure><img className='bg-base-300 rounded-xl' src={logo} alt="Shoes" /></figure>
-            <div className="card-body grid grid-cols-3">
+            <div className="card-body grid grid-cols-2">
                 <h2 className="card-title">{name}</h2>
                 <p className='ml-2 mt-3'><small>Total Q.</small> <b>{total}</b></p>
-                <Link to={`/justify/${id}`}><button className="btn btn-info">Quiz</button></Link>
+            </div>
+            <div>
+                <Link to={`/justify/${id}`}><button className="btn btn-info px-3 ml-32">Start Quiz</button></Link>
             </div>
         </div>
 
